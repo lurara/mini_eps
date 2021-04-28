68ca3a0afd32a754cecf8e5ea9afce66aa4c9651
Lara <laraayumi56@gmail.com>
2021-04-28 19:24:51 -0300

    Mudanças no repositório

--- mini_ep2/old.c
+++ mini_ep2/old.c
@@ -14,7 +14,23 @@
 #include <stdio.h>  //printf, scanf
 #include <stdlib.h> //malloc, free
 
-// implementado: contabilização apenas de ímpares
+// implementados: contabilização apenas de ímpares, bitarray
+// ref. bitarray: http://www.mathcs.emory.edu/~cheung/Courses/255/Syllabus/1-C-intro/bit-array.html
+
+void setbit(int *A, int k) {
+    A[k/32] |= 1 << (k%32);    
+}
+
+int testbit(int *A, int k) {
+    if ((A[k/32] & (1 << (k%32))))
+        return 1;
+    
+    return 0;
+}
+
+void clearbit(int *A, int k) {
+    A[k/32] &= ~(1 << (k%32));    
+}
 
 int main() {
     int N;
@@ -29,24 +45,27 @@ int main() {
     if(N%32) // se for != 0
         crivoSize++;
 
-    int *crivo = malloc((N/2)*sizeof(int));
+    int * crivo = malloc(crivoSize*sizeof(int));
+    //int *crivo = malloc((N/2)*sizeof(int));
     int actual = 0;
 
     for(int i = 0; i <= N/2; i++) {
-        crivo[i] = 1;
+        setbit(crivo, i);
     }
 
     // i representa o número de vdd
     for(int i = 1; i < N/2; i++) {
-        if(crivo[i] == 1) {
+        if(testbit(crivo, i) == 1) {
             primos++;
             actual = (i*2)+1;
+            //printf("%d ", actual);
             
             if((actual)%4 != 3)
                 primosEspeciais++;
             
             for(int j = actual*3; j < N; j = j + 2*actual)
-                crivo[j/2] = 0;
+                clearbit(crivo, j/2);
+                //crivo[j/2] = 0;
         }
     }
 
